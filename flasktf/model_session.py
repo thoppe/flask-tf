b8418182c2458c7dea75c9d3e4256442211797a5
Travis Hoppe <travis.hoppe@gmail.com>
2018-02-19 13:01:39 -0500

    Refactor model session for keras support

--- flasktf/model_session.py
+++ flasktf/model_session.py
@@ -9,7 +9,7 @@ import os
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 
 
-class tfModelSession(object):
+class baseModelSession(object):
 
     def __init__(self, model_func=None):
         self.sess = None
@@ -17,20 +17,17 @@ class tfModelSession(object):
         if model_func is not None:
             self.set_model(model_func)
 
-    def set_model(self, model_func):
+    def configTF(self):
         config = tf.ConfigProto()
         config.gpu_options.allow_growth = True
         self.sess = tf.InteractiveSession(config=config)
 
-        self.var = model_func()
-        if not self.var:
-            raise ValueError("No variables returned by model function.")
-
-        self.sess.run(tf.global_variables_initializer())
-
-    def get_variables(self):
+    def _check_model_set(self):
         if self.sess is None:
             raise ValueError("set_model has not been run")
+
+    def get_variables(self):
+        self._check_model_set()
         return self.var
 
     def __getitem__(self, key):
@@ -51,9 +48,20 @@ class tfModelSession(object):
             val['rank'] = 0
         return val
 
+
+class tfModelSession(baseModelSession):
+
+    def set_model(self, model_func):
+        self.configTF()
+
+        self.var = model_func()
+        if not self.var:
+            raise ValueError("No variables returned by model function.")
+
+        self.sess.run(tf.global_variables_initializer())
+
     def __call__(self, *targets, **feed_args):
-        if self.sess is None:
-            raise ValueError("set_model has not been run")
+        self._check_model_set()
 
         feed_dict = {self[k]: v for k, v in feed_args.items()}
         target_vars = [self[k] for k in targets]
